9f0872b29813b03b7ad3119a66f8aff5cf86901f
skchronicles <kuhnsa2@vcu.edu>
2020-12-10 18:06:09 -0500

    Adding new script to generate TIN matrix due to memory issue (uses ~5000x less memory)

--- src/create_tin_matrix.py
+++ src/create_tin_matrix.py
@@ -1,34 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 from __future__ import print_function
-from functools import reduce
-import pandas, os, sys
+import sys, pandas, os
 
 
-def filepath2samplename(s):
-	bn = os.path.basename(s)
-	sn = bn.split(".p2.Aligned.")[0]
-	return sn
+def create(file, tin_dict, key_index=0, parse_index=4):
+	"""Populates the TIN nested dictionary
+	@param file <str>: Path to RSEQC output file with TIN values to extract
+	@param tin_dict <dict>: Dictionary to populate where [samplebasename][transcriptid] = tin_value
+	@param key_index <int>: Index of the field to join multiple files
+	@param parse_index <int>: Index of field of interest (i.e. TIN value)
+	"""
+
+	with open(file, 'r') as fh:
+		header = next(fh).strip().split('\t')
+		colid = header[key_index]
+		file = os.path.basename(file) # Remove PATH
+		sample = file.split(".p2.Aligned.")[0]
+
+		for line in fh:
+			linelist = line.strip().split('\t')
+			tid = linelist[key_index]
+			tinvalue = linelist[parse_index]
+			if sample not in tin_dict:
+				tin_dict[sample] = {}
+
+			tin_dict[sample][tid] = tinvalue
+
+	return colid, tin_dict
 
 
-def read_tin_xls(s,sn):
-	df = pandas.read_csv(s,sep="\t",header=0,usecols=['geneID','TIN'])
-	df.columns=['geneID', sn]
-	return df
-
-
-def readit(s):
-	return read_tin_xls(s,filepath2samplename(s))
 
 
 if __name__ == '__main__':
 
+	# Get filenames to parse
+	args = sys.argv
+	files = sys.argv[1:]
 
-	files=sys.argv[1:]
+	# Check if at least two files were provided
+	if not len(args) >= 2:
+		print("FATAL: Failed to provide more than one input file!")
+		sys.exit("Usage:\n python {} *.tin.xls".format(args[0]))
 
-	if not files:
-		# User did not provide a list of files to merge!
-		print("Usage Error: Failed to provide any files\npython {} /path/to/TIN/output/*tin.xls".format(sys.argv[0]))
-		sys.exit(1)
+	# Populate tins with TINS values for all transcripts across all samples
+	tins = {}
+	for file in files:
+		keycolname, tins = create(file, tins)
 
-	df=reduce(lambda a,b:pandas.merge(a,b), map(lambda x:read_tin_xls(x, filepath2samplename(x)), files))
-	df.to_csv("combined_TIN.tsv", sep="\t", header=True, index=False)
-
+	# Save tins as a tsv file: combined_TIN.tsv
+	df = pandas.DataFrame(tins)
+	df.to_csv("combined_TIN.tsv", sep="\t", header=True, index=True, index_label = keycolname)
